a0a45f3e47341352e9c1434f7168aeff597f1232
rararaMiranda <144435377+rararaMiranda@users.noreply.github.com>
2025-01-25 13:28:01 +0700

    update nodemon

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 4000
+const port = process.env.APP_PORT || 4000
 
 app.listen((port, () => {
   console.log(`Server is running on port ${port}`)
